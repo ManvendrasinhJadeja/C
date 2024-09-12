6a138df8b512ca2454e9fcdd9545e99179eacc4d
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-09-12 15:32:14 +0530

    Day-8

---
+++ Day-8/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int start = 1;
+
+    do  
+    {
+        printf("%d",start); 
+        start++ ;
+        }
+    while (start <= 10);
+    
+    
+    
+}
---
+++ Day-8/3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int start = 1,n;
+    printf("please enter the value of n: ");
+    scanf("%d",&n);
+
+    do  
+    {
+       if (start%2 == 0)
+       {
+        printf("%d",start);
+       }
+       else{printf(" ");}
+       start++;}
+    while ( start<=n);
+  
+ 
+    
+    
+    
+}

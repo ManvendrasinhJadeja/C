813756f223d88daa3b6d6b4351c0d635dbd14106
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2024-09-12 18:52:04 +0530

    Day-8.2

--- fibo/1.c
+++ fibo/1.c
@@ -3,16 +3,14 @@
 
 int main(){
     int a,b,c,end;
-    a=1;
+    a=-1;
     b=1;
     
     
     // 0 1 1 2 3 5 8 13 21 34 55....
     printf("Please enter the value of n : ");
     scanf("%d",&end);
-     printf("0 ");
-     printf("%d ",a);
-     printf("%d ",b);
+    
 
    
    for ( c=a+b; c <= end; )
